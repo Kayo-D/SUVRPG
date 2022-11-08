663491b11b422fe8ba87ced9f2230d6480c6dc1e
Simon <simoncarlstein@live.se>
2022-11-08 12:59:06 +0100

    Added menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SUVRPG
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string title = "Welcome to SUVRPG!";
+            
+            string[] options = { "NEW GAME", 
+            "LOAD GAME", 
+            "HIGHSCORE", 
+            "QUIT"
+            };
+
+            Menu mainMenu = new Menu(title, options);
+            int SelectedIndex = mainMenu.Run();
+
+            switch (SelectedIndex)
+            {
+                case 0:
+                    break;
+                
+                case 1:
+                    break;
+                
+                case 2:
+                    break;
+                
+                case 3:
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+    }
+}
