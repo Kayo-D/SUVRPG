f964a909c381ca77131e06980ea26e6295e4bf22
bellefix <bellefixgustafsson@outlook.com>
2022-11-08 14:24:21 +0100

    CharUI updated + Char class = Player

---
+++ Logik/Player.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SUVRPG
+{
+    public class Player
+    {
+        string? name { get; set; }
+        string? race { get; set; }
+        string? characterDescription { get; set; }    
+
+        public void CharacterInfo(string name, string race, string characterDescription)
+        {
+
+        }
+    }
+}
--- UI/CharacterUI.cs
+++ UI/CharacterUI.cs
@@ -7,6 +7,36 @@ namespace SUVRPG
 {
     public class CharacterUI
     {
-        
+        public static void characterCreation()
+        {
+            Console.Clear();
+            Player character = new Player();
+
+            Console.WriteLine("CHARACTER CREATION\n");
+            Console.WriteLine("The name of your character: ");
+            string? name = Console.ReadLine();
+            
+            while (true)
+            {
+                if (name == "")
+                {
+                    Console.WriteLine("Please enter a valid character name: ");
+                    name = Console.ReadLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("PLEASE ENTER YOUR CHOICE OF CHARACTER RACE:\n");
+            Console.WriteLine("- HUMAN");
+            Console.WriteLine("- ORC");
+            Console.WriteLine("- ELF");
+            Console.WriteLine("- DWARF");
+            string? race = Console.ReadLine();
+
+            string? characterDescription = Console.ReadLine();
+            character.CharacterInfo(name, race, characterDescription);
+        }
     }
 }
