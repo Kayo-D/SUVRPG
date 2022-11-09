d1edeea968ae60b9accf20475aebd6e5366df859
Christian <cw94@live.se>
2022-11-09 15:44:37 +0100

    ini

--- UI/ShopUI.cs
+++ UI/ShopUI.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SUVRPG
+public class ShopUI
 {
-    public class ShopUI
-    {
-        
-    }
+
 }
