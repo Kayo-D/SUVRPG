880d76ebd474410614cac34fb15d1a1c45c7e733
Kayo-D <113675667+Kayo-D@users.noreply.github.com>
2022-11-20 13:57:53 +0100

    fixed up load and savegame ui

--- UI/SaveGameUI.cs
+++ UI/SaveGameUI.cs
@@ -3,9 +3,13 @@ namespace SUVRPG
 {
     public class SaveGameUI
     {
-        public void OpenSaveGameUI(Player player)
+        DB db = new();
+        public void OpenSaveGameUI(Player player, LevelManager manager, TileEngine engine)
         {
             Clear();
+            //Make this into a single transaction
+            //db.SavePlayer(player);
+            //db.SaveLevelMap(manager.currentLevel, manager.levelData, engine.currentPlayerPosX, engine.currentPlayerPosY);
             WriteLine("Succesfully saved game");
             WriteLine("In order to load game search for your characters name: " + player.Name);
             WriteLine("Press any key to go back");
