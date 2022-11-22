b43a0f64f6903a52cec82e3bcf9d32dd948ae877
Simon <simoncarlstein@live.se>
2022-11-22 14:19:02 +0100

    Update to enemies

--- Program.cs
+++ Program.cs
@@ -6,10 +6,8 @@
         private static void Main(string[] args)
         {     
 
-              //Testa inom dessa
-            DB db = new();
-            Player player = new("Izza","Elf","Likes IKEA", 30, 26, 25, 14, 6, ConsoleColor.Black);
-            db.SavePlayer(player);
+            //Testa inom dessa
+            MainMenu.Mainmenu();
             //Testa inom dessa 
 
         //   MainMenu.Mainmenu();
