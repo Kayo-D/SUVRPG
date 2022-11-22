7583ec4d0e3c1de50749e280b6b04d11e1b5ddb9
bellefix <bellefixgustafsson@outlook.com>
2022-11-22 14:02:39 +0100

    databas trams

--- Program.cs
+++ Program.cs
@@ -5,14 +5,7 @@
 
         private static void Main(string[] args)
         {     
-
-              //Testa inom dessa
-            DB db = new();
-            Player player = new("Izza","Elf","Likes IKEA", 30, 26, 25, 14, 6, ConsoleColor.Black);
-            db.SavePlayer(player);
-            //Testa inom dessa 
-
-        //   MainMenu.Mainmenu();
+            MainMenu.Mainmenu();
         }
     }
 }
