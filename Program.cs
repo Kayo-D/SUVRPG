234e8dcdeb76751573a40e3860167d68ee6e05d1
bellefix <bellefixgustafsson@outlook.com>
2022-11-22 14:52:52 +0100

    nothing changed

--- Program.cs
+++ Program.cs
@@ -7,13 +7,7 @@
         {     
 
             //Testa inom dessa
-<<<<<<< HEAD
-            DB dB = new DB();
-            dB.LoadLevelMap(dB.LoadPlayer("Belle"));
-         
-=======
             MainMenu.Mainmenu();
->>>>>>> 5bd7350dd49ec1c79e8dac0ce6433f84f6fd08d8
             //Testa inom dessa 
 
             //MainMenu.Mainmenu();
