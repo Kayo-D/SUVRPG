a943614b786b4fd7ad346e1bb3621a70887ec540
bellefix <bellefixgustafsson@outlook.com>
2022-11-22 13:00:11 +0100

    currentgold is currentgold

--- Logik/DB.cs
+++ Logik/DB.cs
@@ -26,9 +26,8 @@ public class DB
     {
         Connection().Query($"INSERT INTO player (name, characterDescription, hitpoints, maxhitpoints, armor, attackdmg, race, currentGold) VALUES ('{player.Name}', '{player.CharacterDescription}', '{player.Health}', '{player.MaxHealth}', '{player.Armor}', '{player.AttackDmg}', '{player.Race}', '{player.CurrentGold}');");
     }
-
 }
-    /*public LoadedLevel LoadLevelMap(string input)
+ /*   public LoadedLevel LoadLevelMap(string input)
     {
         LoadedLevel loadedLevel = new();
         var levelMapList = Connection().Query<LoadedLevel>($"SELECT currentLevel, mapData, playerStartPosX, playerStartPosY FROM leveldata;").ToList();
@@ -44,7 +43,8 @@ public class DB
         }
         return loadedLevel;
     } 
-    public void SaveLevelMap(int currentLevel, string[,] mapData, int playerStartPosX, int playerStartPosY)
+}
+  /*  public void SaveLevelMap(int currentLevel, string[,] mapData, int playerStartPosX, int playerStartPosY)
     {
         Connection().Query($"INSERT INTO leveldata (currentlevel, mapData, playerStartPosX, playerStartPosY) VALUES ({currentlevel}, '{mapData}', {playerStartPosX}, {playerStartPosY});");
     }
